6acd39b53b211e24333393386c03772bb53554ce
PedroSergey <pedrosergey.datascientist@gmail.com>
2024-04-30 15:42:02 +0100

    adding a new project

--- day14-higherlower.py
+++ day14-higherlower.py
@@ -1,10 +1,11 @@
 # import the necessary libraries
 
 import random
-from day14_data import data
-from ascii_images import day14_vs as vs, day14_logo as logo
+import os
+from project_data.day14_data import data
+from project_data.ascii_images import day14_vs as vs, day14_logo as logo
 
-# define the functions to pick a name from the list and print it
+# define the function to pick a name from the list and return it
 
 def pick_a_name(data_list):
     '''Pick a element from a list and remove this element from the actual list'''
@@ -12,16 +13,10 @@ def pick_a_name(data_list):
     random_pick = random.randint(0, len(data_list) - 1)
     random_person = data_list[random_pick]
 
-    data_list.pop(random_pick)
-
-    
-
     return random_person
 
-def first_comparison():
-
-    person_a = pick_a_name(data)
-    person_b = pick_a_name(data)
+def comparison(data, person_a, person_b):
+    """Perform a comparison between two people"""
 
     print(f"Compare A: {person_a['name']}, a {person_a['description']} from {person_a['country']}.")
     print(vs)
@@ -30,21 +25,50 @@ def first_comparison():
     followers_a = person_a['follower_count']
     followers_b = person_b['follower_count']
 
-    if followers_a > followers_b:
+    guess = input("Who do you think have more followers? 'A' or 'B': ").lower()
+
+    if followers_a > followers_b and guess == "a":
+        print(f"\nYou are right. {person_a['name']} has {person_a['follower_count']} million followers and {person_b['name']} has {person_b['follower_count']} million.")
         return person_a
-    elif followers_a < followers_b:
+    elif followers_a < followers_b and guess == "b":
+        print(f"\nYou are right. {person_a['name']} has {person_a['follower_count']} million followers and {person_b['name']} has {person_b['follower_count']} million.")
         return person_b
     else:
+        print(f"\nYou are wrong! {person_a['name']} has {person_a['follower_count']} million followers and {person_b['name']} has {person_b['follower_count']} million.")
         return "c"
+
 
 # print the start of the game
 
-# print(logo)
+print(logo, "\nWelcome to this guessing game!")
 
-
-# define some initial variables 
 
 
 # start the game
 
-print(comparison())
+def play_comparison():
+
+    person_a = pick_a_name(data)
+    person_b = pick_a_name(data)
+
+    data.remove(person_a)
+    data.remove(person_b)
+
+    result = comparison(data, person_a, person_b)
+
+    while result != "c":
+        person_b = pick_a_name(data)
+        data.remove(person_b)
+        result = comparison(data, result, person_b)
+
+    if result == "c":
+        print("You have lost!")
+
+    play_again = input("Do you want to play again? Type 'y' or 'n': ").lower()
+
+    if play_again == 'y':
+        os.system('CLS')
+        play_comparison()
+
+play_comparison()
+print("Thanks for having fun with us! See you next time <|3")
